15c8f69a2f86928f27bb029d469e99b4d129d179
maxixking66 <maxixking66@gamil.com>
2024-10-10 15:57:45 +0330

    complete main app

--- project/main-app/src/main/java/ir/maktabsharif115/springboot/Application.java
+++ project/main-app/src/main/java/ir/maktabsharif115/springboot/Application.java
@@ -1,8 +1,13 @@
 package ir.maktabsharif115.springboot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "ir.maktabsharif115.springboot")
 public class Application {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(Application.class, args);
     }
+
 }
