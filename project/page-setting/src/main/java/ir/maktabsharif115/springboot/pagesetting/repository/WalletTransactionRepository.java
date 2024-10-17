9a4609027fea2dd7d73dcd897c217076e887627e
maxixking66 <maxixking66@gamil.com>
2024-10-17 11:58:11 +0330

    add mongo query to file

--- project/page-setting/src/main/java/ir/maktabsharif115/springboot/pagesetting/repository/WalletTransactionRepository.java
+++ project/page-setting/src/main/java/ir/maktabsharif115/springboot/pagesetting/repository/WalletTransactionRepository.java
@@ -6,4 +6,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WalletTransactionRepository extends BaseDocumentRepository<WalletTransaction, String> {
+
+//    Page<WalletTransaction> findAllByUserId(Long userId, Pageable pageable);
+
+//    @Query(value = "{userId: ?1}")
+//    Page<WalletTransaction> findAllByUserIdCustom(Long userId, Pageable pageable);
 }
